4f277bf6d38ff6e472357847e01cf7fdb9e285f0
David Linhardt <divad2012@web.de>
2025-01-23 03:26:38 +0100

    specify engine

--- generatePdf.sh
+++ generatePdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-pandoc ./summary.md -o ./summary.pdf --toc --toc-depth=2
+pandoc ./summary.md -o ./summary.pdf --toc --toc-depth=2 --pdf-engine=pdflatex
 echo "Generated PDF"
